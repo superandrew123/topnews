e3affb3a1b088f5032f8e9645687677062def476
superandrew123 <andrewmiksch@gmail.com>
2024-07-23 13:42:17 -0400

    removing comment from helper spec

--- spec/helpers/article_helper_spec.rb
+++ spec/helpers/article_helper_spec.rb
@@ -41,17 +41,3 @@ RSpec.describe ArticleHelper, type: :helper do
     end
   end
 end
-
-# module ArticleHelper
-#   def star_classes(article, current_user)
-#     if article.users.include? current_user
-#       'article__star article__star--selected'
-#     else
-#       'article__star'
-#     end
-#   end
-#   def article_liked_by(article)
-#     article.users.map(&:full_name).to_sentence
-#   end
-# end
-
